ecb06d6964d971ac81ddb6a5522832f1aafe0d3f
jqnfxa <ivborisov@stud.etu.ru>
2025-03-17 02:04:13 +0300

    [update]: build

--- install.sh
+++ install.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# check for Python 3.12
-if ! command -v python3.12 &> /dev/null; then
-    echo "[-] error: Python 3.12 is not installed."
-    exit 1
-fi
-
 # create a virtual environment
 echo "creating venv..."
-python3.12 -m venv .venv
+python -m venv .venv
 
 # cctivate the virtual environment
 echo "activating venv..."
